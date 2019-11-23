cd6a94987c7c9b2ea019b39c65840fde4a35e296
Glitch (hello-express) <none>
2019-11-23 22:10:56 +0000

    💑🌼 Checkpoint ./server.js:8815681/3088 ./package.json:8815681/103

--- server.js
+++ server.js
@@ -1,22 +1,80 @@
-// server.js
-// where your node app starts
+import express from 'express';
+import Expo from 'expo-server-sdk';
 
-// init project
-const express = require("express");
 const app = express();
+const expo = new Expo();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+let savedPushTokens = [];
+const PORT_NUMBER = 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+const handlePushTokens = (message) => {
+  // Create the messages that you want to send to clents
+  let notifications = [];
+  for (let pushToken of savedPushTokens) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+    notifications.push({
+      to: pushToken,
+      sound: 'default',
+      title: 'Message received!',
+      body: message,
+      data: { message },
+    })
+  }
+
+  // The Expo push notification service accepts batches of notifications so
+  // that you don't need to send 1000 requests to send 1000 notifications. We
+  // recommend you batch your notifications to reduce the number of requests
+  // and to compress them (notifications with similar content will get
+  // compressed).
+  let chunks = expo.chunkPushNotifications(notifications);
+
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let receipts = await expo.sendPushNotificationsAsync(chunk);
+        console.log(receipts);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+}
+
+const saveToken = (token) => {
+  if (savedPushTokens.indexOf(token === -1)) {
+    savedPushTokens.push(token);
+  }
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Push Notification Server Running');
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+app.post('/token', (req, res) => {
+  saveToken(req.body.token.value);
+  console.log(`Received push token, ${req.body.token.value}`);
+  res.send(`Received push token, ${req.body.token.value}`);
+});
+
+app.post('/message', (req, res) => {
+  handlePushTokens(req.body.message);
+  console.log(`Received message, ${req.body.message}`);
+  res.send(`Received message, ${req.body.message}`);
+});
+
+app.listen(PORT_NUMBER, () => {
+  console.log(`Server Online on Port ${PORT_NUMBER}`);
 });
