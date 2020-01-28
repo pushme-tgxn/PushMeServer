c6124b3c32aaa36c3e908447abc504a63ffdd62e
Glitch (easy-soup) <none>
2020-01-28 03:24:51 +0000

    🎋🐄 Checkpoint ./server.js:8815681/846

--- server.js
+++ server.js
@@ -9,18 +9,13 @@ let savedPushTokens = [];
 const PORT_NUMBER = 3000;
 
 const handlePushTokens = ({ title, body }) => {
-  // Create the messages that you want to send to clents
   let notifications = [];
   for (let pushToken of savedPushTokens) {
-    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-
-    // Check that all your push tokens appear to be valid Expo push tokens
     if (!Expo.isExpoPushToken(pushToken)) {
       console.error(`Push token ${pushToken} is not a valid Expo push token`);
       continue;
     }
 
-    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
     notifications.push({
       to: pushToken,
       sound: "default",
@@ -29,18 +24,10 @@ const handlePushTokens = ({ title, body }) => {
       data: { body }
     });
   }
-
-  // The Expo push notification service accepts batches of notifications so
-  // that you don't need to send 1000 requests to send 1000 notifications. We
-  // recommend you batch your notifications to reduce the number of requests
-  // and to compress them (notifications with similar content will get
-  // compressed).
+  
   let chunks = expo.chunkPushNotifications(notifications);
 
   (async () => {
-    // Send the chunks to the Expo push notification service. There are
-    // different strategies you could use. A simple one is to send one chunk at a
-    // time, which nicely spreads the load out over time:
     for (let chunk of chunks) {
       try {
         let receipts = await expo.sendPushNotificationsAsync(chunk);
