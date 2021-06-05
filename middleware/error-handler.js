4c6be16b789004a80e197053cda9bad80bf0a6ed
Glitch (easy-soup) <none>
2021-06-05 19:18:16 +0000

    🐔🎍 Checkpoint ./middleware/error-handler.js:31009658/54

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,11 +6,11 @@ function errorHandler(err, req, res, next) {
       // custom application error
       const is404 = err.toLowerCase().endsWith("not found");
       const statusCode = is404 ? 404 : 400;
-      return res.status(statusCode).json({ message: err });
+      return res.status(statusCode).json({ success: false, message: err });
     case err.name === "UnauthorizedError":
       // jwt authentication error
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ success: false, message: "Unauthorized" });
     default:
-      return res.status(500).json({ message: err.message });
+      return res.status(500).json({ success: false, message: err.message });
   }
 }
