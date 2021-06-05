3dcc17b9acea382df49f22cd8a2011d6883cf5fa
Glitch (easy-soup) <none>
2021-06-05 07:14:23 +0000

    🙌🌹 Checkpoint ./lib/token.js:31009658/505 ./routes.js:31009658/415

--- routes.js
+++ routes.js
@@ -25,10 +25,19 @@ router.get("/list", async (request, response) => {
   response.status(200).send(JSON.stringify(tokenList));
 });
 
-router.post("/register", async (request, response) => {
+router.post("/token", async (request, response) => {
   console.log(`Received push token, ${request.body.token}, ${request.body.name}`);
   
   const savedToken = await saveToken(request.body.token, request.body.name);
+
+  response.json({
+    success: savedToken
+  });
+});
+router.delete("/token", async (request, response) => {
+  console.log(`Received push token, ${request.body.token}, ${request.body.name}`);
+  
+  const savedToken = await removeToken(request.body.token);
 
   response.json({
     success: savedToken
