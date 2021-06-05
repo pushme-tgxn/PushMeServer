9dbecf209cb6e0bb1a0526f6f78e9af8e457b1e4
Glitch (easy-soup) <none>
2021-06-05 03:50:26 +0000

    😂🍎 Checkpoint ./package.json:31009658/737 ./routes/index.js:31009658/46 ./models/index.js:31009658/194 ./routes.js:31009658/4418 ./server.js:31009658/1600 ./lib/token.js:31009658/1064

--- routes.js
+++ routes.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 
-const db = require('./models/index.js');
+const db = require("./models/index.js");
+const {
+  saveToken,
+  updateToken,
+  removeToken,
+  triggerPush
+} = require("./lib/token.js");
 
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
   console.log("Push Notification Server Running");
   response.sendFile(__dirname + "/views/index.html");
 });
 
-router.post("/token", (request, response) => {
-  
+router.post("/token", async (request, response) => {
   saveToken(request.body.token);
-  
+
   console.log(`Received push token, ${request.body.token.value}`);
   response.send(`Received push token, ${request.body.token.value}`);
 });
 
-router.post("/message", (request, response) => {
-  
-  handlePushTokens(request.body);
-  
+router.post("/message", async (request, response) => {
+  triggerPush(request.body);
+
   console.log(`Received message, with title: ${request.body.title}`);
   response.send(`Received message, with title: ${request.body.title}`);
 });
-
 
 module.exports = router;
