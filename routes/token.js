b99ba084869a4e59d0eb31673d54397d1677c5d9
Glitch (easy-soup) <none>
2021-06-07 09:17:36 +0000

    🚝🍝 Checkpoint ./routes/token.js:31009658/5108 ./lib/token.js:31009658/700

--- routes/token.js
+++ routes/token.js
@@ -1,37 +1,75 @@
 const express = require("express");
 
+const { Expo } = require("expo-server-sdk");
+const expo = new Expo();
+
 const {
   listTokens,
-  saveToken,
+  createToken,
+  updateToken,
+  findToken,
   removeToken
 } = require("../lib/token.js");
 
-const authorize = require('../middleware/authorize')
+const authorize = require("../middleware/authorize");
 
 const tokenRouter = express.Router();
 
 tokenRouter.get("/", authorize(), async (request, response) => {
   console.log(`get token list`);
-  
+
   const tokenList = await listTokens(request.body.token);
-  
-  response.setHeader('Content-Type', 'application/json');
+
+  response.setHeader("Content-Type", "application/json");
   response.status(200).send(JSON.stringify(tokenList));
 });
 
 tokenRouter.post("/", authorize(), async (request, response) => {
-  console.log(`Received push token, ${request.body.token}, ${request.body.name}`);
-  
-  const savedToken = await saveToken(request.body.token, request.body.name);
+  console.log(
+    `Received push token, ${request.body.token}, ${request.body.name}`
+  );
+
+  const { token, name } = request.body;
+
+  if (!Expo.isExpoPushToken(token)) {
+    console.error(`Push token ${token} is not a valid Expo push token`);
+    throw new Error(`Push token ${token} is not a valid Expo push token`);
+  }
+
+  const foundToken = await findToken(token);
+  if (!foundToken) {
+    
+    const savedToken = await createToken({
+      userId: 1,
+      token
+    });
+
+    return savedToken;
+  } else {
+    const updated = await db.Token.update(
+      {
+        token: pushToken,
+        name: name
+      },
+      { where: { token: pushToken } }
+    );
+const savedToken = await updateToken({
+      userId: 1,
+      token: token
+    });
+    return updated;
+  }
 
   response.json({
     success: savedToken
   });
-  
 });
+
 tokenRouter.delete("/:tokenId", authorize(), async (request, response) => {
-  console.log(`Received push token, ${request.params.tokenId}, ${request.body.name}`);
-  
+  console.log(
+    `Received push token, ${request.params.tokenId}, ${request.body.name}`
+  );
+
   const savedToken = await removeToken(request.params.tokenId);
 
   response.json({
