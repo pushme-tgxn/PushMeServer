c2bbe890306c1ca8f34ecd9a62c86b8306fd1d6b
Domenic Horner <domenic@tgxn.net>
2021-11-06 22:20:24 +0800

    Bootsstrap auth methods

---
+++ models/userauthmethod.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserAuthMethod extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      UserAuthMethod.belongsTo(models.User, {
+        as: "user",
+        foreignKey: "userId",
+      });
+
+      UserAuthMethod.addScope("defaultScope", {
+        include: [
+          {
+            model: models.User,
+            as: "user",
+            attributes: { exclude: ["token"] },
+          },
+        ],
+      });
+    }
+  }
+  UserAuthMethod.init(
+    {
+      method: DataTypes.STRING,
+      methodIdent: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "UserAuthMethod",
+    }
+  );
+  return UserAuthMethod;
+};
