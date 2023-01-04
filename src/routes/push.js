ae449fc307cd2139671d27cc2f13f4e0b836628d
Domenic Horner <domenic@tgxn.net>
2023-01-04 19:22:28 +0800

    clarify poll

--- src/routes/push.js
+++ src/routes/push.js
@@ -14,7 +14,7 @@ router.post("/:topicSecret", createPushRequest);
 
 // get information on a push request
 router.post("/:pushIdent/response", recordPushResponse); // when someone taps/clicks response on a push
-router.get("/:pushIdent/status", getPushStatus); // to get response data
+router.get("/:pushIdent/status", getPushStatus); // to get current response data
 router.get("/:pushIdent/poll", getPushStatusPoll); // poll till there is a response
 
 module.exports = router;
